cfae86fd6a47b83b3acd6ed7a7f157797498bd83
Ivo Krivzov <ivokrivzov@gmail.com>
2021-05-31 14:32:30 -0300

    acomodado login

--- pages/login.js
+++ pages/login.js
@@ -24,7 +24,7 @@ const clearErrors = () =>{
   const handleLogin = () =>{
     clearErrors();
     firebase
-      .auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+      .auth()
       .signInWithEmailAndPassword(email, password)
       .catch(err => {
         switch(err.code){
